e545cf365304947f3429cc55b26360573b292352
Nick <ndloucks@proton.me>
2025-10-14 23:07:12 -0700

    add more types plus methods for calculating win threshold

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import(
-
+	"fmt"
+	"math"
 )
 
 type TournamentParticipant struct {
@@ -13,28 +14,62 @@ type TournamentParticipant struct {
 	LogoPath:	string	`json:"logo_path"`
 }
 
-type MatchType uint8
+type SeriesType uint8
 
 const (
-	BestOfOne 	MatchType = 1,
-	BestOfThree MatchType = 3,
-	BestOfFive 	MatchType = 5,
-	BestOfSeven MatchType = 7,
-	MFV			MatchType = 0, // use this MatchType when overriding the default MarginForVictory
+	Single		 	SeriesType = 1,
+	BestOfThree		SeriesType = 3,
+	BestOfFive 		SeriesType = 5,
+	BestOfSeven 	SeriesType = 7,
+	SingleMFV		SeriesType = 0, // use this SeriesType when overriding the default MarginForVictory
 )
-
-type SetType uint8
 
 type MarginForVictory uint8
 
+type GameStatus string
+
+const (
+	Upcoming	GameStatus = "upcoming",
+	InProgress	GameResult = "in-progress",
+	Final		GameResult = "final",
+)
+
+type GameResult struct {
+	Winner:		TournamentParticipant		`json:"winner"`,
+	Loser:		TournamentParticipant		`json:"loser"`,
+	WinScore:	uint8						`json:"win_score"`,
+	LoseScore:	uint8						`json:"lose_score"`,
+	GameStatus:	GameStatus					`json:"game_status"`,
+}
+
+type MatchSet struct {
+	Games:		[]GameResult		`json:"games"`,
+}
+
 type Match struct {
-	MatchType:				MatchType = 1			`json:"match_type" default:"1"`, // must be MFV (0) if overriding the Match's MarginForVictory attribute
-	SetType:				SetType	= 1				`json:"set_type" default:"1"`,
-	MarginForVictory:		MarginForVictory = 1	`json:"margin_for_victory" default:"1"`
-	HigerSeedParticipant:	TournamentParticipant	`json:"higher_seed"`,
-	LowerSeedParticipant:	TournamentParticipant	`json:"lower_seed"`,
+	MatchType:				SeriesType = 1			`json:"match_type" default:"1"`, // must be MFV (0) if overriding the Match's MarginForVictory attribute
+	SetType:				SeriesType = 1			`json:"set_type" default:"1"`,
+	MatchMFV:				MarginForVictory = 1	`json:"match_margin_for_victory" default:"1"`,
+	SetMFV:					MarginForVictory = 1	`json:"set_margin_for_victory" default:"1"`,
+	Sets:					[]MatchSet				`json:"sets"`,
+	HigerSeedParticipant:	TournamentParticipant	`json:"higher_seed"`, // higher seed meens a "better" participant and thus a lower number
+	LowerSeedParticipant:	TournamentParticipant	`json:"lower_seed"`, // lower seed means a "worse" participant and this a higher number
 }
 
 type TournamentBracket struct {
-	// A tree-like data structure containing GameOrMatch objects
+	// A tree-like data structure containing Match objects
+}
+
+func (m *Match) CalcWinThreshold() (uint8, error) {
+	if m.MatchType == 0 {
+		return 0, fmt.Errorf("unsupported match type")
+	}
+	return uint8(math.Ceil(m.MatchType / 2))
+}
+
+func (s *MatchSet) CalcWinThreshold() (uint8, error) {
+	if s.SetType == 0 {
+		return 0, fmt.Errorf("unsupported set type")
+	}
+	return uint8(math.Ceil(s.SetType / 2))
 }
