5ec838d591b4703e13d02eca4d51e9a7d170b428
Nick <ndloucks@proton.me>
2025-10-22 20:44:44 -0700

    tested FinalizeGame

--- entities/game.go
+++ entities/game.go
@@ -1,6 +1,9 @@
 package entities
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type GameStatus uint8
 
@@ -11,15 +14,26 @@ const (
 )
 const UnknownGameStatus = "unkown game status"
 
-type GameResult struct {
-	Winner     TournamentParticipant `json:"winner"`
-	Loser      TournamentParticipant `json:"loser"`
-	WinScore   uint8                 `json:"win_score"`
-	LoseScore  uint8                 `json:"lose_score"`
-	GameStatus GameStatus            `json:"game_status"`
+// The participant and their final score in a game, set, or match
+type ParticipantOutcome struct {
+	Participant TournamentParticipant `json:"participant"`
+	Score       uint8                 `json:"score"`
 }
 
-// Displays a string representation of the game state. Uses a value-receiver as it is intended to be used for read-only.
+type OutcomePair struct {
+	FavoriteOutcome ParticipantOutcome `json:"favorite_outcome"`
+	UndedogOutcome  ParticipantOutcome `json:"underdog_outcome"`
+}
+
+type GameResult struct {
+	Winner     ParticipantOutcome `json:"winner"`
+	Loser      ParticipantOutcome `json:"loser"`
+	GameStatus GameStatus         `json:"game_status"`
+	TieAllowed bool               `json:"tie_allowed"`
+}
+
+// Displays a string representation of the game state. 
+// Uses a value-receiver as it is intended to be used for read-only.
 func (gs GameStatus) String() string {
 	switch gs {
 	case Upcoming:
@@ -33,7 +47,8 @@ func (gs GameStatus) String() string {
 	}
 }
 
-func (gr *GameResult) UpdateGameStatus(gs GameStatus) error {
+// 
+func (gr *GameResult) SetGameStatus(gs GameStatus) error {
 	if gs.String() == UnknownGameStatus {
 		return fmt.Errorf("%v", UnknownGameStatus)
 	}
@@ -41,7 +56,34 @@ func (gr *GameResult) UpdateGameStatus(gs GameStatus) error {
 	return nil
 }
 
-func (gr *GameResult) FinalizeGame() {
-	gr.UpdateGameStatus(Final)
+type SimpleError string
 
+var (
+	ErrImpermissibleTie = errors.New("impermissible tie")
+)
+
+func (e SimpleError) Error() string {
+	return string(e)
+}
+
+
+// Updates the game status and declares the winner and loser.
+// If the final score is tied AND a tie is permitted for this game, the "winner" will be the participant with the better seed.
+func (gr *GameResult) FinalizeGame(resultPair OutcomePair) error {
+	if resultPair.FavoriteOutcome.Score == resultPair.UndedogOutcome.Score {
+		if gr.TieAllowed {
+			gr.Winner = resultPair.FavoriteOutcome
+			gr.Loser = resultPair.UndedogOutcome
+		} else {
+			return ErrImpermissibleTie
+		}
+	} else if resultPair.FavoriteOutcome.Score > resultPair.UndedogOutcome.Score {
+		gr.Winner = resultPair.FavoriteOutcome
+		gr.Loser = resultPair.UndedogOutcome
+	} else { // underdog has higher score
+		gr.Winner = resultPair.UndedogOutcome
+		gr.Loser = resultPair.FavoriteOutcome
+	}
+	gr.SetGameStatus(Final)
+	return nil
 }
--- entities/game_test.go
+++ entities/game_test.go
@@ -2,14 +2,16 @@ package entities
 
 import (
 	"testing"
+	"reflect"
+	// "github.com/nickdloucks/tournaments/tournamenterrors"
 )
 
-func TestUpdateGameStatus(t *testing.T) {
+func TestSetGameStatus(t *testing.T) {
 	t.Run("using known GameResult type", func(t *testing.T) {
 		testGR := GameResult{
 			GameStatus: Upcoming,
 		}
-		testGR.UpdateGameStatus(Final)
+		testGR.SetGameStatus(Final)
 		got := testGR.GameStatus
 		want := Final
 
@@ -21,7 +23,7 @@ func TestUpdateGameStatus(t *testing.T) {
 		testGR := GameResult{
 			GameStatus: InProgress,
 		}
-		got := testGR.UpdateGameStatus(255)
+		got := testGR.SetGameStatus(255)
 		want := UnknownGameStatus
 
 		if got.Error() != want {
@@ -30,10 +32,87 @@ func TestUpdateGameStatus(t *testing.T) {
 	})
 }
 
-// func TestFinalizeGame(t *testing.T) {
+func TestFinalizeGame(t *testing.T) {
+	t.Run("using tie score", func(t *testing.T) {
+		testPOs := [2]ParticipantOutcome{
+			{Participant: TournamentParticipant{}, Score: 0}, 
+			{Participant: TournamentParticipant{}, Score: 0},
+		}
+		testPair := OutcomePair{
+			FavoriteOutcome: testPOs[0],
+			UndedogOutcome: testPOs[1],
+		}
+		testGR := GameResult{
+			TieAllowed: false,
+		}
+		got := testGR.FinalizeGame(testPair)
+		want := ErrImpermissibleTie
+		if (got == nil) {
+			t.Errorf("impermissible tie score: %v-%v. got %s want %s", testGR.Winner.Score, testGR.Loser.Score, got, want.Error())
+		} else if (got != want){
+			t.Errorf("impermissible tie score: %v-%v. got %s want %s", testGR.Winner.Score, testGR.Loser.Score, got.Error(), want.Error())
+		}
+	})
+	t.Run("winner selection based on score", func(t *testing.T) {
+		testPOs := [2]ParticipantOutcome{
+			{Participant: TournamentParticipant{Name: "team-with-seed1-and-score0", Seed: 1}, Score: 0},
+			{Participant: TournamentParticipant{Name: "team-with-seed2-and-score2", Seed: 2}, Score: 2},
+		}
+		testPair := OutcomePair{
+			FavoriteOutcome: testPOs[0],
+			UndedogOutcome: testPOs[1],
+		}
+		testGR := GameResult{
+			TieAllowed: false,
+		}
+		testGR.FinalizeGame(testPair)
+		got := testGR.Winner
+		want := testPOs[1]
+		
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got incorrect winner: got  %v, want %v", got.Participant.Name, want.Participant.Name)
+		}
+	})
+	t.Run("winner selection based on seed", func(t *testing.T) {
+		testPOs := [2]ParticipantOutcome{
+			{Participant: TournamentParticipant{Name: "team-with-seed1-and-score0", Seed: 1}, Score: 0},
+			{Participant: TournamentParticipant{Name: "team-with-seed2-and-score0", Seed: 2}, Score: 0},
+		}
+		testPair := OutcomePair{
+			FavoriteOutcome: testPOs[0],
+			UndedogOutcome: testPOs[1],
+		}
+		testGR := GameResult{
+			TieAllowed: true,
+		}
+		testGR.FinalizeGame(testPair)
+		got := testGR.Winner
+		want := testPOs[0]
+		
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got incorrect winner: got  %v, want %v", got.Participant.Name, want.Participant.Name)
+		}
+	})
+	t.Run("status of GameResult", func(t *testing.T) {
+		testPOs := [2]ParticipantOutcome{
+			{Participant: TournamentParticipant{Name: "team-with-seed1-and-score0", Seed: 1}, Score: 0},
+			{Participant: TournamentParticipant{Name: "team-with-seed2-and-score0", Seed: 2}, Score: 0},
+		}
+		testPair := OutcomePair{
+			FavoriteOutcome: testPOs[0],
+			UndedogOutcome: testPOs[1],
+		}
+		testGR := GameResult{
+			GameStatus: InProgress,
+			TieAllowed: true,
+		}
+		testGR.FinalizeGame(testPair)
+		got := testGR.GameStatus
+		want := Final
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 
 
-// 	// if got != want {
-// 	// 	t.Errorf("got %q want %q", got, want)
-// 	// }
-// }
+}
