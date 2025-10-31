b35d7b806bc1d2935acf9dbf906c0ab8d0e0d52b
Nick <ndloucks@proton.me>
2025-10-31 13:40:51 -0700

    better field names

--- entities/match.go
+++ entities/match.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Match struct {
-	MatchType            SeriesType            `json:"match_type" default:"1"` // must be MFV (0) if overriding the Match's MarginForVictory attribute
-	MFVInMatch           MarginForVictory      `json:"margin_for_victory_in_match" default:"1"`
-	MFVInSet             MarginForVictory      `json:"margin_for_victory_in_set" default:"1"`
-	Sets                 map[uint8]MatchSet    `json:"sets"`
-	HigerSeedParticipant TournamentParticipant `json:"higher_seed"` // higher seed meens a "better" participant and thus a lower number
-	LowerSeedParticipant TournamentParticipant `json:"lower_seed"`  // lower seed means a "worse" participant and this a higher number
+	MatchType      SeriesType            `json:"match_type" default:"1"` // must be MFV (0) if overriding the Match's MarginForVictory attribute
+	MFVInMatch     MarginForVictory      `json:"margin_for_victory_in_match" default:"1"`
+	MFVInSet       MarginForVictory      `json:"margin_for_victory_in_set" default:"1"`
+	Sets           map[uint8]MatchSet    `json:"sets"`
+	HomeOrFav      TournamentParticipant `json:"home_or_favored"`  // higher seed meens a "better" participant and thus a lower number
+	AwayOrUnderdog TournamentParticipant `json:"away_or_underdog"` // lower seed means a "worse" participant and this a higher number
 }
 
 func (m *Match) CalcWinThreshold() (uint8, error) {
